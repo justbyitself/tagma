a33cf6a2359206195ba14b515821c4c450d7c079
justbyitself <justbyitself@proton.me>
2026-01-08 22:48:14 +0100

    Improve normalize

--- src/css/normalize.js
+++ src/css/normalize.js
@@ -1,9 +1,7 @@
-export default (css) =>
-  css
-    .replace(/\s*{\s*/g, '{')   // Eliminar espacios antes y después de {
-    .replace(/\s*}\s*/g, '}')   // Eliminar espacios antes y después de }
-    .replace(/\s*:\s*/g, ':')   // Eliminar espacios antes y después de :
-    .replace(/\s*;\s*/g, ';')   // Eliminar espacios antes y después de ;
-    .replace(/\n\s*/g, '')      // Eliminar saltos de línea y espacios después
-    .replace(/^\s+|\s+$/g, '')  // Trim al inicio y final
-    .replace(/\s+/g, ' ');      // Reemplazar múltiples espacios por uno solo
+export default (css) => css
+  .replace(/\s+/g, ' ')
+  .replace(/\s*{\s*/g, '{')
+  .replace(/\s*}\s*/g, '}')
+  .replace(/\s*:\s*/g, ':')
+  .replace(/\s*;\s*/g, ';')
+  .trim()
