0fabfabe561aebda5dac9d1bc8699e71ef31347e
justbyitself <justbyitself@proton.me>
2026-01-09 10:06:13 +0100

    Refactor scheme

--- src/css/themes/scheme.js
+++ src/css/themes/scheme.js
@@ -1,18 +1,15 @@
-import rule from '../rule.js'
+import root from '../root.js'
+import declare from '../declare.js'
 import media from '../media.js'
-import toVarName from '../toVarName.js'
 
 export default (theme, defaultMode = 'light') => {
   const altMode = defaultMode === 'light' ? 'dark' : 'light'
-  const allKeys = new Set([...Object.keys(theme.light), ...Object.keys(theme.dark)])
 
-  const getProps = (mode) => Array.from(allKeys).reduce((acc, key) => {
-    acc[toVarName(key)] = theme[mode][key] ?? theme[defaultMode][key]
-    return acc
-  }, {})
+  const defaultTheme = theme[defaultMode]
+  const altTheme = { ...theme[defaultMode], ...theme[altMode] }
 
-  const base = rule(':root')(getProps(defaultMode))
-  const alt = rule(':root')(getProps(altMode))
-
-  return `${base}\n${media(`(prefers-color-scheme: ${altMode})`)(alt)}`
+  return `
+    ${root(declare(defaultTheme))}
+    ${media(`(prefers-color-scheme: ${altMode})`)(root(declare(altTheme)))}
+    `
 }
